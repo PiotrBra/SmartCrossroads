fd8169d909f026e6c3d20ada5858bd220428707d
Piotr Branewski <pbranewski@student.agh.edu.pl>
2025-03-09 16:59:00 +0100

    lightState update

--- src/main/java/org/example/model/enums/LightState.java
+++ src/main/java/org/example/model/enums/LightState.java
@@ -1,7 +1,18 @@
 package org.example.model.enums;
 
 public enum LightState {
-    GREEN,
-    YELLOW,
-    RED
+    RED("czerwone"),
+    YELLOW("żółte"),
+    GREEN("zielone");
+
+    private final String description;
+
+    LightState(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
 }
